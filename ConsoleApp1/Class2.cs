5fb7da57cffe7c3c0e0c5f449ef00ecf0edf01ee
fukhan <fukhan389@hotmail.com>
2019-08-02 20:26:26 +1000

    Added new class2 in TestBranch

---
+++ ConsoleApp1/Class2.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    [TestClass]
+    public class Class2
+    {
+        [TestMethod]
+
+        public void Method1()
+
+        {
+            Console.WriteLine("This is Test Branch Class");
+        }
+
+    }
+}
