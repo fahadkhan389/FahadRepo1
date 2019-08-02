980afdd35d83fbfd83db0aebdbb1a95388b700df
fukhan <fukhan389@hotmail.com>
2019-08-02 19:52:19 +1000

    Added Class1

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+
+        public void Method1()
+
+        {
+            Console.WriteLine("This is method1");
+        }
+
+    }
+}
