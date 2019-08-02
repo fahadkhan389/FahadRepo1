4421651dfd109644ab85e1849f3654eacbb2f327
fukhan <fukhan389@hotmail.com>
2019-08-02 19:29:52 +1000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string[] abc = GetNames();
+            Console.WriteLine(abc[2]);
+            
+            
+
+        }
+
+        public static string[] GetNames()
+        {
+
+          string[] xyz= new string[] { "Sunday", "Monday", "Tuesday" };
+            return xyz;
+        }
+
+    }
+}
